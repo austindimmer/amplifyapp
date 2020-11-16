a123c9ee8a252bd694ab44f58908703fea06f0b9
Eric Descoteaux <eric.descoteaux@cpu.ca>
2020-11-15 23:54:17 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.png';
+import logo from './logo.svg';
 import './App.css';
 import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
@@ -13,7 +13,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test de login cognito
         </p>
         <a
           className="App-link"
