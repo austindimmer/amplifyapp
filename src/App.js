05729156895ec1eb2f0b6a6d36ae81f32ecbb366
Eric Descoteaux <eric.descoteaux@cpu.ca>
2020-11-16 00:07:20 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
+// import awsconfig from './aws-exports';
 import React from 'react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
+const awsmobile = {
+  "aws_project_region": "ca-central-1",
+  "aws_cognito_identity_pool_id": "ca-central-1:1ceef8f8-b6ed-4178-a483-be0edcff76e7",
+  "aws_cognito_region": "ca-central-1",
+  "aws_user_pools_id": "ca-central-1_5mg8qywhU",
+  "aws_user_pools_web_client_id": "6e1rd9d695539v3rrqruegirop",
+  "oauth": {}
+};
+
 
 Amplify.configure(awsconfig)
 
